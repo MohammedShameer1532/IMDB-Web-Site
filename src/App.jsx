0bc9212aae88c9f0e2600d536daac588bff19d22
Mohammed Shameer <118889168+MohammedShameer1532@users.noreply.github.com>
2023-11-11 12:54:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
 import Get from "./Get";
-const App =()=>{
-  const [search,setSearch] = useState("");
-  const [getData,setGetData] =useState([]);
 
-  const handleChangeEvent=(event)=>{
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [getData, setGetData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChangeEvent = (event) => {
     setSearch(event.target.value);
   };
-  const handleClick =()=>{
-   
-    const apikey = 'aa903cd7';
-   
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${apikey}&s=${search}`)
-    .then((response)=>response.json())
-    .then((data)=>{
-      console.log(data);
-      setGetData(data.Search);
-    });
-   
-  }
 
-  return(
+  const handleClick = () => {
+    setLoading(true);
+    setError(null);
+
+    const apiKey = 'aa903cd7';
+
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${search}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setGetData(data.Search);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setError('An error occurred while fetching data.');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
     <div>
-         <Input onChange={handleChangeEvent}/>
+      <Input onChange={handleChangeEvent} />
 
-    <Button onClick={handleClick}/>
+      <Button onClick={handleClick} />
 
-    <Get getData={getData} />
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
 
-    
+      <Get getData={getData} />
     </div>
-  )
-}
+  );
+};
+
 export default App;
